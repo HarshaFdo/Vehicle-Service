6adee45c5d4845afd5193bf06349c838cb1b622a
Harsha Fernando <fernando.harsha2016@gmail.com.com>
2025-11-03 21:39:21 +0530

    update input dto

--- src/vehicle/dto/update-vehicle.input.ts
+++ src/vehicle/dto/update-vehicle.input.ts
@@ -1,22 +1,35 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsString, IsEmail, IsOptional, IsDateString } from 'class-validator';
 
 @InputType()
 export class UpdateVehicleInput {
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   first_name?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   last_name?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsEmail()
   email?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   car_make?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   car_model?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsDateString()
   manufactured_date?: Date;
 }
